46eb6114ee31e4be2413af51b30ec7ed7e33ca45
crist <cristiandf41@gmail.com>
2019-04-17 22:55:58 +0200

    initial

--- app/src/main/java/com/example/progettoappandroid/InsertActivity.java
+++ app/src/main/java/com/example/progettoappandroid/InsertActivity.java
@@ -1,16 +1,19 @@
 package com.example.progettoappandroid;
 
-import android.app.FragmentTransaction;
+//import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
-//import android.support.v4.app.Fragment;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
+
 
 
 public class InsertActivity extends AppCompatActivity {
@@ -41,13 +44,36 @@ public class InsertActivity extends AppCompatActivity {
         F_A_B.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                FloatingActionButton remove = findViewById(R.id.remove_frag);
+                remove.show();
+                remove.setEnabled(true);
                 FragmentManager fm = getSupportFragmentManager();
-                Exercise_Fragment newCase = new Exercise_Fragment();
-                FragmentTransaction transaction=fm.beginTransaction();
-                transaction.add(R.id.fragment, newCase);// give your fragment container id in first parameter
+                FragmentTransaction transaction = fm.beginTransaction();
+                Exercise_Fragment f = new Exercise_Fragment();
+                transaction.setCustomAnimations(R.animator.fade_in,R.animator.fade_out);
+                transaction.add(R.id.Linear, f, "tag");// give your fragment container id in first parameter
                 //transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
                 transaction.commit();
 
+             }
+        });
+
+        final FloatingActionButton remova_frag = findViewById(R.id.remove_frag);
+        remova_frag.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view) {
+                FragmentManager managers = getSupportFragmentManager();
+                Fragment f = managers.findFragmentByTag("tag");
+                FragmentTransaction ft = managers.beginTransaction();
+                ft.setCustomAnimations(R.animator.fade_in,R.animator.fade_out);
+                if (f != null){
+                    ft.remove(f);
+                    ft.commit();
+
+                } else {
+                    remova_frag.hide();
+                    remova_frag.setEnabled(false);
+                }
             }
         });
     }
